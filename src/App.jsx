6e6f43a0c324074c5f5c1a25611721dcf36ebc64
emrul hassan biplob <hemrul494@gmail.com>
2025-09-01 22:53:51 +0300

    started

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex p-20 w-full bg-slate-200 h-screen">
       <h1 className="text-3xl font-bold  text-blue-500">
-        Starting a new project.
+        Successfully Starting a new project.
       </h1>
     </div>
   );
